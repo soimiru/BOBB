4a6b1001863e6a97a3615b408dd1c62177830fbe
Sonia <so.milagro.2018@alumnos.urjc.es>
2021-12-25 13:00:52 +0100

    Funcionalidad inicial
    -Menu principal
    -Bola moviéndose

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private GameObject levelSelector, mainMenu;
+
+    private void Awake()
+    {
+        mainMenu = GameObject.Find("MainMenuPanel");
+        levelSelector = GameObject.Find("LevelSelectorPanel");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        levelSelector.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoLevelSelector() {
+        mainMenu.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    public void GoToLevel(string level) {
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rigBody;
+    float dirX;
+    float speed = 25f;
+
+    private void Awake()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        dirX = Input.acceleration.x * speed;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y, transform.position.z);
+    }
+    void FixedUpdate()
+    {
+        rigBody.velocity = new Vector3(dirX, -5f, 0f);
+    }
+}
